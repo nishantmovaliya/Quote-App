d44cf32d580cd40f387672ca4388978fa66472bb
Nishant Movaliya <63806205+nishantmovaliya@users.noreply.github.com>
2021-09-04 11:59:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quoteapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.quoteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button share;
+    TextView quote;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        share = findViewById(R.id.btnshare);
+        quote = findViewById(R.id.tvquote);
+
+        getSupportActionBar().hide();
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sharetxt = quote.getText().toString();
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.setType("text/plain");
+                sendIntent.putExtra(Intent.EXTRA_TEXT , sharetxt);
+                startActivity(sendIntent);
+            }
+        });
+    }
+}
